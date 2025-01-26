a70078f59e8721d9991711f1c318e7bdda6357f4
PEDDAKOTLA GAMANA DATTA <63487087+GamanaDatta@users.noreply.github.com>
2025-01-26 21:35:51 +0530

    GetMethod With Headers Tested

--- src/main/java/com/qa/client/RestClient.java
+++ src/main/java/com/qa/client/RestClient.java
@@ -2,6 +2,7 @@ package com.qa.client;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.http.Header;
 import org.apache.http.client.ClientProtocolException;
@@ -18,6 +19,22 @@ public class RestClient {
 	public CloseableHttpResponse get(String url) throws ClientProtocolException, IOException {
 		CloseableHttpClient HttpClient = HttpClients.createDefault();
 		HttpGet httpget = new HttpGet(url); //http get request
+		CloseableHttpResponse chr = HttpClient.execute(httpget); //hit the get url
+		
+		return chr;
+		
+	}
+	
+
+	//GetMethod with headers
+	public CloseableHttpResponse get(String url, HashMap<String, String>hashMap) throws ClientProtocolException, IOException {
+		CloseableHttpClient HttpClient = HttpClients.createDefault();
+		HttpGet httpget = new HttpGet(url); //http get request
+		
+		for(Map.Entry<String, String> entry :hashMap.entrySet() ) {
+			httpget.addHeader(entry.getKey(), entry.getValue());
+		}  
+		
 		CloseableHttpResponse chr = HttpClient.execute(httpget); //hit the get url
 		
 		return chr;
--- src/test/java/com/qa/tests/GetAPItest.java
+++ src/test/java/com/qa/tests/GetAPItest.java
@@ -2,6 +2,7 @@ package com.qa.tests;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.http.Header;
 import org.apache.http.client.ClientProtocolException;
@@ -36,7 +37,67 @@ public class GetAPItest extends TestBase{
 		}
 	
 	@Test
-	public void getTest() throws ClientProtocolException, IOException{
+	public void getAPITestwithoutHeaders() throws ClientProtocolException, IOException{
+	
+		RestClient restClient = new RestClient();
+		
+		
+		HashMap<String, String> headerMap = new HashMap<String,String>();
+		headerMap.put("Content-Type","application/json");
+		//headerMap.put("username","gamana@nt.com");
+		//headerMap.put("password","pass123");
+		
+		chr = restClient.get(url,headerMap);
+		
+				//status code
+				int statusCode = chr.getStatusLine().getStatusCode();
+				System.out.println("status code : "+statusCode);
+				
+				Assert.assertEquals(statusCode, RESPONSE_STATUS_CODE_200, "Status code is not 200");
+				
+				//json response 
+				String ResponseString = EntityUtils.toString(chr.getEntity(), "UTF-8");
+				JSONObject responseJson = new JSONObject(ResponseString);
+				System.out.println("JSON Response : "+responseJson);
+				
+				//single value assertion from json response
+				String perpagevalue = TestUtil.getValueByJPath(responseJson, "/per_page");
+				System.out.println("value of per page is-->"+ perpagevalue);
+				Assert.assertEquals(Integer.parseInt(perpagevalue), 6);
+				
+				//total:
+				String totalValue = TestUtil.getValueByJPath(responseJson, "/total");
+				System.out.println("value of total is-->"+ totalValue);		
+				Assert.assertEquals(Integer.parseInt(totalValue), 12);
+
+				//get the value from JSON ARRAY:
+				String lastName = TestUtil.getValueByJPath(responseJson, "/data[0]/last_name");
+				String id = TestUtil.getValueByJPath(responseJson, "/data[0]/id");
+				String avatar = TestUtil.getValueByJPath(responseJson, "/data[0]/avatar");
+				String firstName = TestUtil.getValueByJPath(responseJson, "/data[0]/first_name");
+
+				System.out.println(lastName);
+				System.out.println(id);
+				System.out.println(avatar);
+				System.out.println(firstName);
+				
+				
+				//headers
+				Header[] headersArray = chr.getAllHeaders();
+				
+				HashMap<String,String> allHeaders = new HashMap<String,String>();
+				for(Header header : headersArray) {
+					
+					allHeaders.put(header.getName(), header.getValue());
+					
+				}
+				
+				System.out.println(" Headers array : "+allHeaders);
+						
+	}
+
+	@Test
+	public void getAPITestwithHeaders() throws ClientProtocolException, IOException{
 	
 		RestClient restClient = new RestClient();
 		chr = restClient.get(url);
@@ -85,6 +146,12 @@ public class GetAPItest extends TestBase{
 				}
 				
 				System.out.println(" Headers array : "+allHeaders);
+				
+			
+				
+			}
 	}
 
-}
+	
+
+
